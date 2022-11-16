339bc976666254dbd8527cf951ed8e18ee5bc8e9
Yannick Stoll <yannick.stoll@pixelforest.io>
2022-11-16 13:41:43 +0100

    First tests in test_utils.py with the associated data.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,27 @@
+import pickle
+
+import icecream as ic
+import numpy as np
 import pytest
 
-from easyocr.utils import consecutive, word_segmentation
+from easyocr.utils import (BeamEntry, BeamState, addBeam, consecutive,
+                           ctcBeamSearch, word_segmentation)
 
-# data: [28 29 40 41]
-# mode: last
-# result consecutive: [29, 41]
 
-result_cons = consecutive(data= [28, 29, 40, 41, 50] , mode="first", stepsize=1)
-print(f"result_cons: {result_cons}")
+# Fixtures:
+@pytest.fixture
+def load_fr_dict():
+    with open("easyocr/dict/fr.txt", "r") as dict_fr:
+        return dict_fr.read().splitlines()
 
+@pytest.fixture
+def load_mat_probs_mairie():
+    out_pickle = "tests/golden_utils/data/mat_proba_Mairie.csv"
+    return np.genfromtxt(out_pickle, delimiter=',')
+
+
+# Tests:
+# "Normal"
 def test_consecutive():
     data = [28, 29, 40, 41]
     mode = "last"
@@ -18,8 +31,26 @@ def test_consecutive():
     mode="first"
     assert consecutive(data=data, mode=mode, stepsize=1) == [28, 40, 50]
 
+# "Golden": inputs are read in Yaml file, except for dict or matrices
+# which are either read from txt file (dict) or unpickle (matrice)
+
 @pytest.mark.golden_test("golden_utils/test_word_segmentation.yaml")
 @pytest.mark.parametrize("test", ["test_1", "test_2", "test_3"])
 # @pytest.mark.parametrize("test", ["test_2"])
 def test_word_segmentation(golden, test):
     assert word_segmentation(**golden[test]["input"]) == golden.out[test]["output"]
+
+
+@pytest.mark.golden_test("golden_utils/test_ctcBeamSearch.yaml")
+def test_ctcBeamSearch(golden, load_fr_dict, load_mat_probs_mairie):
+    fr_dict = load_fr_dict
+    mat_probs = load_mat_probs_mairie
+    result = ctcBeamSearch(mat_probs,
+                           golden["input"]["classes"],
+                           golden["input"]["ignore_idx"],
+                           lm=golden["input"]["ignore_idx"],
+                           beamWidth=golden["input"]["beamWidth"],
+                           dict_list=fr_dict)
+
+
+    assert result == golden.out["output"]
