af3eb871dbf1a232483d0c1874a9acb8f9a24191
Yannick Stoll <yannick.stoll@pixelforest.io>
2022-11-22 11:25:10 +0100

    add marker slow to test_read_text_simple

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -6,7 +6,7 @@ from pytest_golden.plugin import GoldenTestFixture
 from easyocr import Reader
 from tests.conftest import EasyOCROutput
 
-
+@pytest.mark.slow
 @pytest.mark.golden_test("data/test_detection/test_readtext_simple.yml")
 def test_readtext_simple(reader: Reader, golden: GoldenTestFixture) -> None:
     """Ensure the inference engine runs default text detection."""
